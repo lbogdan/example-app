fab44620709ca1d3813078efc8a9a85d9eb14927
Bogdan Luca <luca.bogdan@gmail.com>
2023-10-16 23:31:43 +0300

    fix: dynamically import app

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { isMainThread } from 'node:worker_threads';
 
-import { app } from './app.js';
 import { hash } from './hash.js';
 
 const entrypoint = new URL(import.meta.url).pathname;
 
 if (isMainThread) {
+  const { app } = await import('./app.js');
   app(entrypoint);
 } else {
   hash();
